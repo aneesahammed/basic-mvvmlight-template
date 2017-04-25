7ffc8cac1655800a8f698d594a6d1f5de9b8584b
AneesAhammed <aneesahammed@gmail.com>
2017-04-25 12:05:21 +0400

    #Initial check-in

---
+++ MvvmLightApp/MvvmLightApp/App.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Windows;
+using GalaSoft.MvvmLight.Threading;
+using Microsoft.Practices.ServiceLocation;
+using MvvmLightApp.ViewModel;
+
+namespace MvvmLightApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        #region <-PrivateVars->
+        #endregion
+
+        #region <-Constructor->
+        static App()
+        {
+            try
+            {
+                DispatcherHelper.Initialize();
+                ViewModelLocator.Init();
+            }
+            catch (Exception)
+            {   
+                throw; //#TODO:log exception
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+        }
+        
+        #endregion
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            AppInit();
+        }
+
+        private void AppInit()
+        {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            DispatcherUnhandledException += LibraryApplication_DispatcherUnhandledException;
+
+            LoadWindow();
+        }
+
+        private static void LoadWindow()
+        {
+            var shellWindow = ServiceLocator.Current.GetInstance<MainWindow>();
+            shellWindow.Topmost = true;
+            shellWindow.Show();
+            shellWindow.Topmost = false;
+        }
+
+        #region <-PrivateMethods->
+        #endregion
+
+        #region <-ApplicaitonEventHandling->
+        void LibraryApplication_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+
+        }
+
+        void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ MvvmLightApp/MvvmLightApp/SingleInstanceManager.cs
@@ -0,0 +1,68 @@
+﻿/*
+ * @file:SingleInstanceManager
+ * @brief: for single instance of application.
+ * @author:AA
+ * @Credits:http://www.switchonthecode.com/tutorials/wpf-writing-a-single-instance-application                
+ */
+
+using System;
+using Microsoft.VisualBasic.ApplicationServices;
+
+namespace MvvmLightApp
+{
+    public sealed class SingleInstanceManager:WindowsFormsApplicationBase
+    {
+        #region <-Properties->
+        public App App { get; private set; }
+        #endregion
+
+        #region <-Constructor->
+        private SingleInstanceManager()
+        {
+            IsSingleInstance = true;
+        }
+        #endregion
+
+
+        #region <-Main->
+
+        /// <summary>
+        /// Main method which creates a new SingleInstanceManager and passes in arguments.
+        /// </summary>
+        /// <param name="args"></param>
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            new SingleInstanceManager().Run(args);
+        }
+        #endregion
+        
+
+        #region <-ProtectedMethods->
+
+        /// <summary>
+        /// Overrides the OnStartup Method from Base.
+        /// On first startup, creates a new Application and runs it. 
+        /// </summary>
+        /// <returns>A System.Boolean that indicates if the application should continue starting up.</returns>
+        protected override bool OnStartup(StartupEventArgs eventArgs)
+        {
+            App = new App();
+            App.Run();
+
+            return false;
+        }
+
+        /// <summary>
+        /// Overrides the OnStartupNextInstance Method from Base.
+        /// On subsequent startup, restore the current application instance and passes any command line arguments to it
+        /// </summary>
+        /// <param name="eventArgs"></param>
+        protected override void OnStartupNextInstance(StartupNextInstanceEventArgs eventArgs)
+        {
+                base.OnStartupNextInstance(eventArgs);
+                App.MainWindow.Activate();
+        }
+        #endregion
+    }
+}
